a97aad3e4684119eed2cda08e8aecde68c449202
firefly017 <jui.n.nahar@gmail.com>
2022-09-14 00:01:07 +0200

    fixing bugs, adding styles

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-var operand1 = 0;
-var operand2 = 0;
-var lastOperator = "";
-var displayNum = "";
+let operand1 = 0;
+let operand2 = 0;
+let lastOperator = "";
+let displayNum = "";
 function onNumber(number) {
   if (lastOperator == "") {
     operand1 = operand1 * 10 + number;
@@ -25,9 +25,20 @@ function operate() {
   if (lastOperator == "+") operand1 = operand1 + operand2;
   else if (lastOperator == "-") operand1 = operand1 - operand2;
   else if (lastOperator == "*") operand1 = operand1 * operand2;
-  else if (lastOperator == "/") operand1 = operand1 / operand2;
+  else if (lastOperator == "/") {
+    if (operand2 != 0) {
+      operand1 = operand1 / operand2;
+      operand1 = Math.round((operand1 + Number.EPSILON) * 10000) / 10000;
+    } else {
+      document.getElementById("result").innerHTML =
+        "division by 0 is not allowed";
+      return;
+    }
+  }
   operand2 = 0;
+  lastOperator = "";
   document.getElementById("result").innerHTML = operand1;
+  console.log(operand1);
 }
 
 function clearCalculation() {
